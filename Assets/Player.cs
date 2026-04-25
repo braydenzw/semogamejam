7c3268ef6eceee5c470daf739d44e1edc2b96656
braydenzw <143677822+braydenzw@users.noreply.github.com>
2026-04-24 23:52:04 -0500

    player orientation

---
+++ Assets/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("操你妈")]
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
+    [SerializeField] private Sprite[] PlayerSprite;
+
+    
+        
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            attack(1);
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            attack(0);
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            attack(2);
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            attack(3);
+        }
+            
+    }
+
+    void attack(int 方向)
+    {
+        animator.SetFloat("Direction", (float)方向);
+        spriteRenderer.flipX = (方向 == 2);
+        //animator.SetTrigger("Attack");
+        //check on beat
+    }
+
+}
