ff6bf3dffa32dca5419659750fbc1b153770fb41
Rainbowzilla <xwingstarfighter301@gmail.com>
2026-04-26 06:32:59 -0700

    Credits Menu
    -Got all the names
    -Added Film Grain Background Noises
    -Added Button Click sound effect

--- Assets/Nick's Scripts/MainMenuManager.cs
+++ Assets/Nick's Scripts/MainMenuManager.cs
@@ -6,20 +6,48 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     public float fadeDuration = 5f;
+    public GameObject creditsMenu;
     public void LoadScene(string sceneName)
     {
-        StartCoroutine(FadeOut(sceneName));
+        StartCoroutine(FadeOutToWhite(sceneName));
     }
 
     public void ExitGame()
     {
+        StartCoroutine(FadeOutToBlack());
+    }
+
+    public void TransitionToCredits()
+    {
+        GetComponent<Animator>().SetBool("FadeCredits", true);
+        GetComponent<Animator>().SetBool("FadeToMenu", false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void TransistionFromCreditsToMainMenu()
+    {
+        StartCoroutine(FadeToMenu());
+    }
+
+    IEnumerator FadeOutToWhite(string sceneName)
+    {
+        GetComponent<Animator>().SetTrigger("FadeToWhite");
+        yield return new WaitForSeconds(fadeDuration);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator FadeOutToBlack()
+    {
+        GetComponent<Animator>().SetTrigger("FadeToBlack");
+        yield return new WaitForSeconds(fadeDuration);
         Application.Quit();
     }
 
-    IEnumerator FadeOut(string sceneName)
+    IEnumerator FadeToMenu()
     {
-        GetComponent<Animator>().SetTrigger("FadeToWhite");
-        yield return new WaitForSeconds(5f);
-        SceneManager.LoadScene(sceneName);
+        GetComponent<Animator>().SetBool("FadeCredits", false);
+        GetComponent<Animator>().SetBool("FadeToMenu", true);
+        yield return new WaitForSeconds(3f);
+        creditsMenu.SetActive(false);
     }
 }
