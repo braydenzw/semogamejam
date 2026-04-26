dbb8e35d25a30f9dd8421291bf878934d3cbd001
braydenzw <143677822+braydenzw@users.noreply.github.com>
2026-04-26 01:00:37 -0500

    dsfsd

---
+++ Assets/TestHealthInput.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TestHealthInput : MonoBehaviour
+{
+    public SectionHealth violinSection;   
+
+    void Update()
+    {
+        // fuck it over
+        if (Input.GetKey(KeyCode.Alpha1))
+            violinSection.SetHealth(violinSection.Health - (1f/3f) * Time.deltaTime);
+
+        // heal
+        if (Input.GetKey(KeyCode.Alpha2))
+            violinSection.SetHealth(violinSection.Health + (1f/3f) * Time.deltaTime);
+    }
+}
